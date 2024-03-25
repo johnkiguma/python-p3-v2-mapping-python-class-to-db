f50c6184dbd30ec8c95982df219295051e6fb489
johnkiguma <john.kiguma@student.moringaschool.com>
2024-03-25 11:46:37 +0300

    completed assignment

--- lib/department.py
+++ lib/department.py
@@ -1,12 +1,57 @@
 from __init__ import CURSOR, CONN
 
 
-class Department:
+# department.py
 
-    def __init__(self, name, location, id=None):
-        self.id = id
+class Department:
+    def __init__(self, name, location):
+        # Initialize id attribute to None
+        self.id = None 
         self.name = name
         self.location = location
 
-    def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+    def save(self):
+        # Insert department data into the database and assign the instance an ID
+        CURSOR.execute("INSERT INTO departments (name, location) VALUES (?, ?)", (self.name, self.location))
+        self.id = CURSOR.lastrowid  
+        CONN.commit()
+
+    def update(self):
+        
+        CURSOR.execute("UPDATE departments SET name = ?, location = ? WHERE id = ?", (self.name, self.location, self.id))
+        CONN.commit()
+    
+     # Delete method to update the department record
+    def delete(self):
+        
+        CURSOR.execute("DELETE FROM departments WHERE id = ?", (self.id,))
+        CONN.commit()
+
+    @classmethod
+    #create method to instantiate and save a new department in one step
+    def create(cls, name, location):
+        
+        department = cls(name, location)
+        department.save()  
+        return department
+
+    @staticmethod
+        # Method to create the "departments" table if it doesn't exist
+    def create_table():
+      
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """)
+        CONN.commit()
+
+    @staticmethod
+       # Method to drop the "departments" table if it exists
+    def drop_table():
+        
+        CURSOR.execute("DROP TABLE IF EXISTS departments")
+        CONN.commit()
+
